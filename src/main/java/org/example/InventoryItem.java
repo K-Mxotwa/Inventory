9a7b4143d3ca50051ea450b1111e52b332e1f361
kuhle <kuhlemxotwa00@gmail.com>
2024-09-17 18:00:26 +0200

    Add inventory Item implematation

---
+++ src/main/java/org/example/InventoryItem.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class InventoryItem {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public InventoryItem(String name, int quantity, double price){
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItem{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
